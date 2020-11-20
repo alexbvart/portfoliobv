d2feb29ac44174e88048949085d4a4e2333ee8e5
Alexander Briones <alexanderbvart@gmail.com>
2020-11-19 22:54:57 -0500

    menu responsive

---
+++ js/popup.js
@@ -0,0 +1,34 @@
+/* Toogle menu hamburguesa */
+document.querySelectorAll('.menu').forEach(btn => {
+    btn.addEventListener('click', e => {
+        btn.classList.toggle('active');
+    });
+});
+
+
+
+var btnOpenPopup = document.getElementById('btn-open-popup'),
+    overley = document.getElementById('overley'),
+    popup = document.getElementById('popup');
+
+
+
+btnOpenPopup.addEventListener('click',function(){
+    overley.classList.toggle('active');
+    popup.classList.toggle('active');
+});
+
+/* Seleccionar alguna de la secciones del menu */
+document.querySelectorAll('.poppup__item').forEach(btn => {
+    btn.addEventListener('click', e => {
+        overley.classList.remove('active');
+        popup.classList.remove('active');
+        btn.classList.remove('active');
+    });
+});
+
+
+/* btnClosePopup.addEventListener('click',function(){
+    overley.classList.remove('active');
+    popup.classList.remove('active');
+}); */
